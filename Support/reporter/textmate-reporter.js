c42f7c83203b157c0e65cf88ad99d5d8a7445df8
oost <http://www.github.com/oost>
2013-01-20 22:59:40 -0500

    Use JSHint 1.0.0 RC4 Check style as well as errors

--- Support/reporter/textmate-reporter.js
+++ Support/reporter/textmate-reporter.js
@@ -17,17 +17,47 @@ function sortUnique(arr) {
   return arr;
 }
 
+function render(errors, numErrors) {
+  var style = fs.readFileSync('./views/reporter.css', 'utf8'),
+      script = fs.readFileSync('./views/reporter.js', 'utf8'),
+      content = fs.readFileSync('./views/report.html', 'utf8'),
+      reporterErrorTemplate = fs.readFileSync('./views/reporter-error.html', 'utf8'),
+      template,
+      htmlContent;
+
+  // Load assets
+
+  Handlebars.registerHelper('fileErrors', function(context, options) {
+    var ret = "", file, fileErrors;
+
+    for(file in context) {
+      if (context.hasOwnProperty(file)) {
+        // We are sure that obj[key] belongs to the object and was not inherited.
+        fileErrors = context[ file ];
+        ret = ret + "<li class=\"file\">" +
+                    "<span class=\"desc\">" + file + "</span>" +
+                    "</li>";
+
+        fileErrors.forEach(function (item) { ret = ret + options.fn(item); } );
+      }
+    }
+    return ret;
+  });
+
+  Handlebars.registerPartial('errorTemplate', reporterErrorTemplate);
+
+  template = Handlebars.compile(content);
+  htmlContent = template({errors: errors, numErrors: numErrors,  style: style, script: script});
+
+  process.stdout.write(htmlContent);
+}
+
 module.exports = {
+
   reporter: function (results, data, opts) {
-    var len = results.length,
+    var numErrors = results.length,
         files,
-        errors = {},
-        style = fs.readFileSync('./views/reporter.css', 'utf8'),
-        script = fs.readFileSync('./views/reporter.js', 'utf8'),
-        content = fs.readFileSync('./views/report.html', 'utf8'),
-        reporterErrorTemplate = fs.readFileSync('./views/reporter-error.html', 'utf8'),
-        template,
-        htmlContent;
+        errors = {};
 
 		opts = opts || {};
 
@@ -39,33 +69,59 @@ module.exports = {
           fileErrors = results.filter(function (item) { return (item.file === file); });
 
       // fileErrors = fileErrors.filter(function (item) { return (item.error); });
-      errors[ fileRelative ] = fileErrors.map(function (item) { var o = item.error; o.file = file; return o; });
+      errors[ fileRelative ] = fileErrors.map(function (item) {
+        return {
+          file: file,
+          severity: 'error',
+          line: item.error.line,
+          column: item.error.character,
+          message: item.error.reason,
+          evidence: item.error.evidence,
+          source: item.error.raw
+        };
+      });
     });
 
-    // Load assets
+    // console.log(data);
+    data.forEach(function (item) {
+      var fileRelative = path.relative(process.env.TM_PROJECT_DIRECTORY, item.file),
+          globals = item.implieds,
+          unuseds = item.unused;
 
-    Handlebars.registerHelper('fileErrors', function(context, options) {
-      var ret = "", file, fileErrors;
+      // Register the file
 
-      for(file in context) {
-        if (context.hasOwnProperty(file)) {
-          // We are sure that obj[key] belongs to the object and was not inherited.
-          fileErrors = context[ file ];
-          ret = ret + "<li class=\"file\">" +
-                      "<span class=\"desc\">" + file + "</span>" +
-                      "</li>";
-
-          fileErrors.forEach(function (item) { ret = ret + options.fn(item, {data: data}); } );
-        }
+      if (!errors[fileRelative]) {
+        errors[fileRelative] = [];
       }
-      return ret;
+
+      if (globals) {
+        globals.forEach(function (global) {
+          errors[fileRelative].push({
+            file: item.file,
+            severity: 'warning',
+            line: global.line,
+            column: 0,
+            message: "Implied global '" + global.name + "'",
+            source: 'jshint.implied-globals'
+          });
+        });
+      }
+      if (unuseds) {
+        unuseds.forEach(function (unused) {
+          errors[fileRelative].push({
+            file: item.file,
+            severity: 'warning',
+            line: unused.line,
+            column: 0,
+            message: "Unused variable: '" + unused.name + "'",
+            source: 'jshint.implied-unuseds'
+          });
+        });
+      }
     });
 
-    Handlebars.registerPartial('errorTemplate', reporterErrorTemplate);
+    // console.log
+    render(errors, numErrors);
 
-    template = Handlebars.compile(content);
-    htmlContent = template({errors: errors, numErrors: len,  style: style, script: script});
-
-    process.stdout.write(htmlContent);
 	}
 };
